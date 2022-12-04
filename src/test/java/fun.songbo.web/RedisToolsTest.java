dd1682beeca266ccc2aed24f02e80e4223b29291
jack <songbo2099@gmail.com>
2022-12-04 10:39:19 +0800

    add tests

---
+++ src/test/java/fun.songbo.web/RedisToolsTest.java
@@ -0,0 +1,21 @@
+package fun.songbo.web;
+
+import fun.songbo.web.commons.Expire;
+import fun.songbo.web.config.RedisConfig;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author songbo
+ * @date 2022-12-04 10:55
+ * @since
+ */
+public class RedisToolsTest extends TestCase {
+
+
+
+}
