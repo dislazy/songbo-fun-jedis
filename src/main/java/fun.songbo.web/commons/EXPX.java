50f05cc4ee82427f1d65b7e01f96d722574be1ea
jack <songbo2099@gmail.com>
2022-12-04 10:38:16 +0800

    add some keys

---
+++ src/main/java/fun.songbo.web/commons/EXPX.java
@@ -0,0 +1,30 @@
+package fun.songbo.web.commons;
+
+/**
+ * @author Jack
+ * ex or px
+ */
+public enum  EXPX {
+    /**
+     * ex 秒级
+     */
+    SECONDS("EX"),
+    /**
+     * px 毫秒级
+     */
+    MILLISECONDS("PX");
+
+    private String expx;
+
+    EXPX(String expx) {
+        this.expx = expx;
+    }
+
+    public String getString() {
+        return expx;
+    }
+
+    public byte[] getBytes() {
+        return expx.getBytes();
+    }
+}
---
+++ src/main/java/fun.songbo.web/commons/Expire.java
@@ -0,0 +1,89 @@
+package fun.songbo.web.commons;
+
+/**
+ * @author Jack
+ * expire enum
+ */
+public class Expire {
+    /**
+     * 10秒钟
+     */
+    public static Expire s10 = new Expire(10);
+    /**
+     * 1分钟
+     */
+    public static Expire m1 = new Expire(60);
+    /**
+     * 3分钟
+     */
+    public static Expire m3 = new Expire(180);
+    /**
+     * 5分钟
+     */
+    public static Expire m5 = new Expire(300);
+    /**
+     * 10分钟
+     */
+    public static Expire m10 = new Expire(600);
+    /**
+     * 30分钟
+     */
+    public static Expire m30 = new Expire(1800);
+    /**
+     * 31分钟
+     */
+    public static Expire m31 = new Expire(1860);
+    /**
+     * 1小时
+     */
+    public static Expire h1 = new Expire(3600);
+    /**
+     * 2 小时
+     */
+    public static Expire h2 = new Expire(7200);
+    /**
+     * 6 小时
+     */
+    public static Expire h6 = new Expire(1960);
+    /**
+     * 2小时1分钟
+     */
+    public static Expire h2PlusM1 = new Expire(1860);
+    /**
+     * 1天
+     */
+    public static Expire d1 = new Expire(60 * 60 * 24);
+    /**
+     * 3天
+     */
+    public static Expire d3 = new Expire(60 * 60 * 24 * 3);
+    /**
+     * 7天
+     */
+    public static Expire d7 = new Expire(60 * 60 * 24 * 7);
+    /**
+     * 30天
+     */
+    public static Expire d30 = new Expire(60 * 60 * 24 * 30);
+
+    private final long time;
+    private final EXPX expx;
+
+    public Expire(long time) {
+        this(time, EXPX.SECONDS);
+    }
+
+    public Expire(long time, EXPX expx) {
+        this.time = time;
+        this.expx = expx;
+    }
+
+    public EXPX getEXPX() {
+        return expx;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+}
