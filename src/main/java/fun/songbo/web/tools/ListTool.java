6c336cce1a295bc19c4cf4ef3a8dd2a2920bb551
jack <songbo2099@gmail.com>
2024-01-13 10:42:17 +0800

    add list tool for code data

---
+++ src/main/java/fun/songbo/web/tools/ListTool.java
@@ -0,0 +1,41 @@
+package fun.songbo.web.tools;
+
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ * @author jack
+ */
+public class ListTool {
+    /**
+     * 函数式接口 T -> bollean
+     * @param keyExtractor
+     * @param <T>
+     * @return
+     */
+    public static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+        ConcurrentHashMap<Object, Boolean> map = new ConcurrentHashMap<>(16);
+        return t -> map.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+    }
+
+    public static void removeNullElemnet(List list) {
+        Iterator iterator = list.iterator();
+        while(iterator.hasNext()){
+            if(Objects.isNull(iterator.next())){
+                iterator.remove();
+            }
+        }
+    }
+
+}
