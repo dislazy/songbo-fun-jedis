3b2521042201da8e4404f3114ee46da34f75cc1c
jack <songbo2099@gmail.com>
2024-02-21 22:37:18 +0800

    add ntices tools

--- src/main/java/fun/songbo/web/tools/NoticeTool.java
+++ src/main/java/fun/songbo/web/tools/NoticeTool.java
@@ -3,6 +3,8 @@ package fun.songbo.web.tools;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Method;
+
 /**
  * 消息通知
  */
@@ -11,4 +13,18 @@ public class NoticeTool {
     private static final Logger log = LoggerFactory.getLogger(NoticeTool.class);
     private static String url = "https://open.feishu.cn/open-apis/bot/v2/hook/";
 
+
+    public static Method getGetMethod(Class objectClass, String fieldName) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("get");
+        sb.append(fieldName.substring(0, 1).toUpperCase());
+        sb.append(fieldName.substring(1));
+        try {
+            return objectClass.getMethod(sb.toString());
+        } catch (Exception e) {
+            log.error(e.toString());
+        }
+        return null;
+
+    }
 }
