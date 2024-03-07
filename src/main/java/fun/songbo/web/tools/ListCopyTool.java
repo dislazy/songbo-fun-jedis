8ada4a5e61b77fd488a16f05bec091aa317ebcbc
jack <songbo2099@gmail.com>
2024-03-07 11:09:45 +0800

    add some statistic fun

--- src/main/java/fun/songbo/web/tools/ListCopyTool.java
+++ src/main/java/fun/songbo/web/tools/ListCopyTool.java
@@ -29,4 +29,30 @@ public class ListCopyTool {
         BigDecimal b2 = BigDecimal.valueOf(v2);
         return b1.add(b2).doubleValue();
     }
+
+    /**
+     * 提供精确的减法运算。
+     *
+     * @param v1 被减数
+     * @param v2 减数
+     * @return 两个参数的差
+     */
+    public static double sub(double v1, double v2) {
+        BigDecimal b1 = BigDecimal.valueOf(v1);
+        BigDecimal b2 = BigDecimal.valueOf(v2);
+        return b1.subtract(b2).doubleValue();
+    }
+
+    /**
+     * 提供精确的乘法运算。
+     *
+     * @param v1 被乘数
+     * @param v2 乘数
+     * @return 两个参数的积
+     */
+    public static double mul(double v1, double v2) {
+        BigDecimal b1 = BigDecimal.valueOf(v1);
+        BigDecimal b2 = BigDecimal.valueOf(v2);
+        return b1.multiply(b2).doubleValue();
+    }
 }
