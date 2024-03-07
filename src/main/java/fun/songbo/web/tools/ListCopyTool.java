b79bb90af8463956a319ca409b1b17363500454d
jack <songbo2099@gmail.com>
2024-03-07 11:08:54 +0800

    add add

--- src/main/java/fun/songbo/web/tools/ListCopyTool.java
+++ src/main/java/fun/songbo/web/tools/ListCopyTool.java
@@ -1,6 +1,7 @@
 package fun.songbo.web.tools;
 
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 /**
@@ -16,4 +17,16 @@ public class ListCopyTool {
         return UUID.randomUUID().toString().toUpperCase().replace("-", "");
     }
 
+    /**
+     * 提供精确的加法运算。
+     *
+     * @param v1 被加数
+     * @param v2 加数
+     * @return 两个参数的和
+     */
+    public static double add(double v1, double v2) {
+        BigDecimal b1 = BigDecimal.valueOf(v1);
+        BigDecimal b2 = BigDecimal.valueOf(v2);
+        return b1.add(b2).doubleValue();
+    }
 }
