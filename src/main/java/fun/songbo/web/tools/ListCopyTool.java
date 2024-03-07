53394e0f6ef1f1505668c764d6cb03310862c042
jack <songbo2099@gmail.com>
2024-03-07 11:08:11 +0800

    add UUID

--- src/main/java/fun/songbo/web/tools/ListCopyTool.java
+++ src/main/java/fun/songbo/web/tools/ListCopyTool.java
@@ -1,10 +1,19 @@
 package fun.songbo.web.tools;
 
 
+import java.util.UUID;
+
 /**
  * @author jack
  */
 public class ListCopyTool {
-
+    /**
+     * 生成UUID
+     *
+     * @return UUID
+     */
+    public static String UUID() {
+        return UUID.randomUUID().toString().toUpperCase().replace("-", "");
+    }
 
 }
