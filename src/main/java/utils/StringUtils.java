8e202e247a2c5fcba463e39193e8624cceb5d585
jack <songbo2099@gmail.com>
2024-04-05 14:28:15 +0800

    add string utils

---
+++ src/main/java/utils/StringUtils.java
@@ -0,0 +1,34 @@
+package utils;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author songbo
+ * @date 2024-04-05 14:27
+ * @since
+ */
+public class StringUtils {
+
+    /**
+     * 判断字符串是否为空或仅包含空白字符
+     *
+     * @param str 要检查的字符串
+     * @return 如果字符串为空或仅包含空白字符，则返回true；否则返回false
+     */
+    public static boolean isBlank(String str) {
+        return StringUtils.isBlank(str);
+    }
+
+    /**
+     * 判断字符串是否不为空且不包含空白字符
+     *
+     * @param str 要检查的字符串
+     * @return 如果字符串不为空且不包含空白字符，则返回true；否则返回false
+     */
+    public static boolean isNotBlank(String str) {
+        return StringUtils.isNotBlank(str);
+    }
+
+}
