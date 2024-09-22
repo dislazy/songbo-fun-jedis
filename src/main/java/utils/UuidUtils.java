6d06afff4561fec3776d38765134eea54c3ebc46
jack <songbo2099@gmail.com>
2024-09-22 11:12:35 +0800

    Add methods to generate UUID with dashes, generate short UUID, and validate UUID in UuidUtils class

--- src/main/java/utils/UuidUtils.java
+++ src/main/java/utils/UuidUtils.java
@@ -21,8 +21,40 @@ public class UuidUtils {
         return UUID.randomUUID().toString().toUpperCase().replace("-", "");
     }
 
+    /**
+     * 生成带有连字符的UUID
+     *
+     * @return 带有连字符的UUID
+     */
+    public static String UUIDWithDashes() {
+        return UUID.randomUUID().toString();
+    }
 
-    public static String ShortUUID(){
-       return UUID.fromString("ABCD").toString().toLowerCase();
+    /**
+     * 生成短UUID
+     *
+     * @return 短UUID
+     */
+    public static String ShortUUID() {
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+
+
+    /**
+     * 验证UUID
+     *
+     * @param uuid 要验证的UUID字符串
+     * @return 如果字符串是有效的UUID，则返回true；否则返回false
+     */
+    public static boolean isValidUUID(String uuid) {
+        if (uuid == null) {
+            return false;
+        }
+        try {
+            UUID.fromString(uuid);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
     }
 }
