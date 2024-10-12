ab75ad4d3452b6c6771c1d96bddf6a757eb9753c
jack <songbo2099@gmail.com>
2024-10-12 13:12:35 +0800

    Add methods to get field value, check if class has a specific method, and copy properties in ObjectBaseUtils class

--- src/main/java/utils/ObjectBaseUtils.java
+++ src/main/java/utils/ObjectBaseUtils.java
@@ -3,6 +3,7 @@ package utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 /**
@@ -41,5 +42,63 @@ public class ObjectBaseUtils {
         return null;
     }
 
+
+    /**
+     * 获取字段值
+     *
+     * @param object 目标对象
+     * @param fieldName 字段名称
+     * @return 字段值
+     */
+    public static Object getFieldValue(Object object, String fieldName) {
+        try {
+            Field field = object.getClass().getDeclaredField(fieldName);
+            field.setAccessible(true);
+            return field.get(object);
+        } catch (Exception e) {
+            log.error("[getFieldValue] error: ", e);
+        }
+        return null;
+    }
+
+    /**
+     * 检查类是否具有指定方法
+     *
+     * @param clazz 类
+     * @param methodName 方法名称
+     * @param parameterTypes 方法参数类型
+     * @return 如果类具有指定方法，则返回true；否则返回false
+     */
+    public static boolean hasMethod(Class<?> clazz, String methodName, Class<?>... parameterTypes) {
+        try {
+            clazz.getMethod(methodName, parameterTypes);
+            return true;
+        } catch (NoSuchMethodException e) {
+            return false;
+        }
+    }
+
+    /**
+     * 复制属性
+     *
+     * @param source 源对象
+     * @param target 目标对象
+     */
+    public static void copyProperties(Object source, Object target) {
+        try {
+            Class<?> sourceClass = source.getClass();
+            Class<?> targetClass = target.getClass();
+            Field[] fields = sourceClass.getDeclaredFields();
+            for (Field field : fields) {
+                field.setAccessible(true);
+                Object value = field.get(source);
+                Field targetField = targetClass.getDeclaredField(field.getName());
+                targetField.setAccessible(true);
+                targetField.set(target, value);
+            }
+        } catch (Exception e) {
+            log.error("[copyProperties] error: ", e);
+        }
+    }
 
 }
